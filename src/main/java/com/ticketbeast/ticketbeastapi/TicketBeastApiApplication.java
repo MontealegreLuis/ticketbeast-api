b4018ffda6cb4c1c57af55edcad14da2c3eedb5e
Luis Montealegre <montealegreluis@gmail.com>
2022-02-13 19:39:38 -0600

    chore: Set up Spring Boot application

---
+++ src/main/java/com/ticketbeast/ticketbeastapi/TicketBeastApiApplication.java
@@ -0,0 +1,11 @@
+package com.ticketbeast.ticketbeastapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBeastApiApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(TicketBeastApiApplication.class, args);
+  }
+}
