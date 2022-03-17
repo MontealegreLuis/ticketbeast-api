01bb6e11d727178ad363ccfd490e6f8857e5f3ad
Luis Montealegre <montealegreluis@gmail.com>
2022-03-16 23:41:27 -0500

    test: Specify current profile in controller test

--- src/test/java/com/ticketbeast/ticketbeastapi/rest/concerts/ViewPublishedConcertControllerTest.java
+++ src/test/java/com/ticketbeast/ticketbeastapi/rest/concerts/ViewPublishedConcertControllerTest.java
@@ -13,10 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 final class ViewPublishedConcertControllerTest {
   @Test
   void it_finds_an_upcoming_published_concert() throws Exception {
